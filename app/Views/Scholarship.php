2deb511edea821064101ad27fd660ea0117847e8
LabGich <labradorgichohi@gmail.com>
2023-01-10 11:15:10 +0300

    db added and scholarship changed

--- app/Views/Scholarship.php
+++ app/Views/Scholarship.php
@@ -9,57 +9,57 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
- <?php include 'css.php' ?>
+  <?php include 'css.php' ?>
 
 </head>
 
 <body>
 
   <!-- ======= Header ======= -->
- <?php include 'header.php' ?>
-  
- <main id="main">
+  <?php include 'header.php' ?>
 
-<!-- ======= Breadcrumbs ======= -->
-<div class="breadcrumbs" data-aos="fade-in">
-  <div class="container">
-    <h2>Apply Scholarship</h2>
-    
-  </div>
-</div><!-- End Breadcrumbs -->
+  <main id="main">
 
-<!-- ======= Contact Section ======= -->
-<section id="contact" class="contact">
-  
-  <div class="container card p-3" data-aos="fade-up">
-
-    <div class="row mt-5">
-
-      <div class="col-lg-12 mt-5 mt-lg-0">
-
-        <form action="/Applyscholarship" method="GET" role="form">
-          <div class="row">
-            <div class="col-md-6 form-group">
-              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-            </div>
-            <div class="col-md-6 form-group mt-3 mt-md-0">
-              <input type="text" class="form-control" name="admissionnum" id="admissionnum" placeholder="Your Admission" required>
-            </div>
-          </div>
-          <div class="form-group mt-3">
-            <input type="text" class="form-control" name="reasons" id="reasons" placeholder="Reasons for scholarship" required>
-          </div>
-          <div class="text-center"><button type="submit" class="btn btn-primary mt-2 mb-3">Submit Application</button></div>
-        </form>
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs" data-aos="fade-in">
+      <div class="container">
+        <h2>Apply Scholarship</h2>
 
       </div>
+    </div><!-- End Breadcrumbs -->
 
-    </div>
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
 
-  </div>
-</section><!-- End Contact Section -->
+      <div class="container card p-3" data-aos="fade-up">
 
-</main><!-- End #main -->
+        <div class="row mt-5">
+
+          <div class="col-lg-12 mt-5 mt-lg-0">
+
+            <form action="/Applyscholarship" method="GET" role="form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="text" class="form-control" name="admissionnum" id="admissionnum" placeholder="ID No/Passport/Birth Certificate" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="reasons" id="reasons" placeholder="Reasons for scholarship" required>
+              </div>
+              <div class="text-center"><button type="submit" class="btn btn-primary mt-2 mb-3">Submit Application</button></div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
   <?php include 'footer.php' ?>
--- sql/users.sql
+++ sql/users.sql
@@ -1,15 +1,33 @@
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `userid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
-  `firstname` varchar(50) DEFAULT NULL,
-  `lastname` varchar(50) DEFAULT NULL,  
-  `email` varchar(50) DEFAULT NULL,
-  `phonenum` varchar(50) DEFAULT NULL,
-  `usertype` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `USERS`;
 
-INSERT INTO `users` (`userid`, `username`, `firstname`, `lastname`, `email`, `phonenum`, `usertype`, `password`) VALUES
-	(1, 'ken', 'kenedy', 'cheruiyot', 'cheruiyotkenedy@gmail.com', '0711314358', 'Admin', 'pass');
+CREATE TABLE IF NOT EXISTS `USERS` (
+  `USERID` INT(11) NOT NULL AUTO_INCREMENT,
+  `USERNAME` VARCHAR(50) DEFAULT NULL,
+  `FIRSTNAME` VARCHAR(50) DEFAULT NULL,
+  `LASTNAME` VARCHAR(50) DEFAULT NULL,
+  `EMAIL` VARCHAR(50) DEFAULT NULL,
+  `PHONENUM` VARCHAR(50) DEFAULT NULL,
+  `USERTYPE` VARCHAR(50) DEFAULT NULL,
+  `PASSWORD` VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (`USERID`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=LATIN1;
+
+INSERT INTO `USERS` (
+  `USERID`,
+  `USERNAME`,
+  `FIRSTNAME`,
+  `LASTNAME`,
+  `EMAIL`,
+  `PHONENUM`,
+  `USERTYPE`,
+  `PASSWORD`
+) VALUES (
+  1,
+  'ken',
+  'kenedy',
+  'cheruiyot',
+  'cheruiyotkenedy@gmail.com',
+  '0711314358',
+  'Admin',
+  'pass'
+);
