382806b3ae53599c81eaa63ee5a7783955144780
Kenedy Cheruiyot <kenedy@localhost.localdomain>
2023-01-17 20:15:25 +0300

    lists fix

--- app/Views/userlist.php
+++ app/Views/userlist.php
@@ -8,7 +8,7 @@
   <title>Foreign Language College Mombasa - Login</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-  <link rel='stylesheet' href='https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap.min.css'>
     <link rel='stylesheet' href='https://cdn.datatables.net/fixedheader/3.3.1/css/fixedHeader.bootstrap.min.css'>
     <link rel='stylesheet' href='https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap.min.css'>
  <?php include 'css.php' ?>
@@ -46,10 +46,10 @@
                             <td><?php echo $row['location'] ?></td>
                             <td><?php echo $row['phonenum'] ?></td>
                             <td><?php echo $row['usertype'] ?></td>
-                            <td>
-                                <a href="javascript:void(0)" onclick="getUserDetails('<?= $row['username']; ?>')" title="Edit <?= $row['username']; ?>" data-toggle="modal" data-target="#edit-user-modal">
+                           <td>
+                                <a href="javascript:void(0)" onclick="getUserDetails('<?= $row['ID']; ?>')" title="Edit <?= $row['ID']; ?>" data-toggle="modal" data-target="#edit-user-modal">
                                     <button type="button" class="btn btn-info btn-circle btn-xs">
-                                        <i class="fa fa-eye"></i>
+                                        <i class="bx bxs-bullseye">View</i>
                                     </button>
                                 </a>
 
@@ -63,7 +63,21 @@
         </div>
     </div>
   <?php include 'footer.php' ?>
-
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.4.0/js/responsive.bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/fixedheader/3.3.1/js/dataTables.fixedHeader.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            var table = $('#users-table').DataTable( {
+                responsive: true
+            } );
+        
+            new $.fn.dataTable.FixedHeader( table );
+        } );
+    </script>
 </body>
 
 </html>
