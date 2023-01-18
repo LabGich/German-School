3b525c0c5af45fa5a8aad8f386eebbc9c1917821
Kenedy Cheruiyot <kenedy@localhost.localdomain>
2023-01-18 04:20:24 +0300

    lists check

--- app/Views/scholarshiplist.php
+++ app/Views/scholarshiplist.php
@@ -8,8 +8,9 @@
     <title>Foreign Language College Mombasa - Login</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
-<link rel='stylesheet' href='https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap.min.css'>
-    <link rel='stylesheet' href='https://cdn.datatables.net/fixedheader/3.3.1/css/fixedHeader.bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css'>
+    <link rel='stylesheet' href='https://cdn.datatables.net/buttons/2.3.2/css/buttons.bootstrap5.min.css'>
     <link rel='stylesheet' href='https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap.min.css'>
     <?php include 'css.php' ?>
 
@@ -21,7 +22,7 @@
 
     <div class="row mt-3" style="background: aliceblue;margin-top: 100px !important;">
         <div class=" col-12" style="margin-left:auto; margin-right:auto; padding:50px;">
-            <table id="enrollment-table" class="table table-bordered table-hover" style="margin-left:auto; margin-right:auto; ">
+            <table id="tabledata" class="table table-bordered table-hover" style="margin-left:auto; margin-right:auto; ">
                 <thead>
                     <tr>
                         <th>Admission Number</th>
@@ -56,19 +57,29 @@
     </div>
     <?php include 'footer.php' ?>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>    
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>    
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.bootstrap5.min.js"></script>    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>    
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>    
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>    
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.colVis.min.js"></script>    
     <script src="https://cdn.datatables.net/responsive/2.4.0/js/responsive.bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/fixedheader/3.3.1/js/dataTables.fixedHeader.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script>
-        $(document).ready(function() {
-            var table = $('#enrollment-table').DataTable( {
-                responsive: true
-            } );
-        
-            new $.fn.dataTable.FixedHeader( table );
+       $(document).ready(function() {
+        var table = $('#tabledata').DataTable( {
+            responsive: true,
+            lengthChange: false,
+            buttons: [ 'copy', 'excel', 'pdf', 'colvis' ]
         } );
+        table.buttons().container()
+            .appendTo( '#tabledata_wrapper .col-md-6:eq(0)' );
+    } );
     </script>
 </body>
 
